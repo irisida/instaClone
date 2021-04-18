083d5b66b1f07fe3eae5e5c248d3d3e51d553c5d
irisida <20445648+irisida@users.noreply.github.com>
2021-04-18 20:50:19 +0100

    create the new rootViewController

---
+++ instaclone/Controller/MainTabController.swift
@@ -0,0 +1,17 @@
+//
+//  MainTabController.swift
+//  instaclone
+//
+//  Created by ed on 18/04/2021.
+//
+
+import UIKit
+
+class MainTabController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+    }
+}
---
+++ redme.md
@@ -0,0 +1,3 @@
+# Instagram Clone for iOS 14
+
+Using Swift 5, MVVM pattern and programmatic UI
