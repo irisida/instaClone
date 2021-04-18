0c63ccf67c83f564cbc0fc12e3d0fae3c407dfb4
irisida <20445648+irisida@users.noreply.github.com>
2021-04-18 22:44:55 +0100

    added marks to the main feed controller, created dummy cells

--- instaclone/Controller/FeedController.swift
+++ instaclone/Controller/FeedController.swift
@@ -7,10 +7,45 @@
 
 import UIKit
 
-class FeedController: UIViewController {
+class FeedController: UICollectionViewController {
+    
+    private let reuseIdentifier = "Cell"
+    
+    //MARK: - Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureUI()
         
-        view.backgroundColor = .systemPink
+    }
+    
+    //MARK: - Helper Functions
+    
+    func configureUI() {
+        view.backgroundColor = .white
+        collectionView.backgroundColor = .white
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+    }
+}
+
+//MARK: - UICollectionViewDataSource
+
+extension FeedController  {
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
+        cell.backgroundColor = .systemPink
+        return cell
+    }
+}
+
+//MARK: - UICollectionViewDelegateFlowLayout
+
+extension FeedController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.width, height: 250)
     }
 }
--- instaclone/Controller/MainTabController.swift
+++ instaclone/Controller/MainTabController.swift
@@ -22,10 +22,16 @@ class MainTabController: UITabBarController {
     func configureViewControllers() {
         view.backgroundColor = .white
         
-        let feed = templateNavigationController(unselectedImage: #imageLiteral(resourceName: "home_unselected"), selectedImage: #imageLiteral(resourceName: "home_selected"), rootViewController: FeedController())
+        let layout = UICollectionViewFlowLayout()
+        
+        let feed = templateNavigationController(unselectedImage: #imageLiteral(resourceName: "home_unselected"), selectedImage: #imageLiteral(resourceName: "home_selected"), rootViewController: FeedController(collectionViewLayout: layout))
+        
         let search = templateNavigationController(unselectedImage: #imageLiteral(resourceName: "search_unselected"), selectedImage: #imageLiteral(resourceName: "search_selected"), rootViewController: SearchController())
+        
         let imageSelector = templateNavigationController(unselectedImage: #imageLiteral(resourceName: "plus_unselected"), selectedImage: #imageLiteral(resourceName: "plus_unselected"), rootViewController: ImageSelectorController())
+        
         let notifications = templateNavigationController(unselectedImage: #imageLiteral(resourceName: "like_unselected"), selectedImage: #imageLiteral(resourceName: "like_selected"), rootViewController: NotificationsController())
+        
         let profle = templateNavigationController(unselectedImage: #imageLiteral(resourceName: "profile_unselected"), selectedImage: #imageLiteral(resourceName: "profile_selected"), rootViewController: ProfileController())
  
         viewControllers = [feed, search, imageSelector, notifications, profle]
